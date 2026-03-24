92e40138cdc1d7fc1fb53e3d9106248a47bb822d
catetoadjacente <matheusaraujocunha.56@gmail.com>
2026-03-24 17:41:24 -0300

    tipos de dados

--- ola-mundo.py
+++ ola-mundo.py
@@ -1,3 +1,16 @@
 print("olá mundo")
 print()
 print("matheus")
+
+
+#tipos de dados
+#inteiro  (int)
+inteiro = 1
+#Fracionario (float)
+fracionario = 1.0
+#texto string
+texto = "eu" 
+#logico (Boolean)
+lógico = True
+#vazio
+vazio = None
