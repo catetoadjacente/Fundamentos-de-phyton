3e2fe4a3559526aed6da07ddef1bdeb43dfceaae
catetoadjacente <matheusaraujocunha.56@gmail.com>
2026-03-26 21:01:54 -0300

    relacionais

--- ola-mundo.py
+++ ola-mundo.py
@@ -1,9 +1,9 @@
 print("olá mundo")
-print()
 print("matheus")
 
 
 #tipos de dados
+
 #inteiro  (int)
 inteiro = 1
 #Fracionario (float)
@@ -14,3 +14,35 @@ texto = "eu"
 lógico = True
 #vazio
 vazio = None
+
+
+#operaçoes aritimeticas
+(=)-siginifca "recebe"
+(==)-significa "igual"
+
+var_bool_1 = 3 == 3
+
+# ao printar var bool 1 teremos True já que 3 é igual a 3
+
+(!=)-SIGNIFICA 'desigual'
+
+var_bool_2 = 3 !=6
+# ao printar var bool 2 teremos False
+
+(<, >)- maior e menor que respectivamente
+
+var_bool_3 = 3 < 5
+# True
+var_bool_4 = 3 > 5
+#false
+
+(>=, <=) - maior ou igual que e menor ou igual que
+
+var_bool_5 = 3 <= 3
+#true
+
+var_bool_6 = 3 >= 2
+#false
+
+ 
+print(0 == False > 4-3)
