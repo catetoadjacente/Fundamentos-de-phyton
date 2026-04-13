88b6551dcfd3a60defac483cdf6da056aa24c72f
catetoadjacente <matheusaraujocunha.56@gmail.com>
2026-04-13 20:51:29 -0300

    tkinter

---
+++ tk.py
@@ -0,0 +1,15 @@
+
+import tkinter as tk
+
+janela = tk.Tk()
+janela.geometry("400x300")
+
+button = tk.Button(janela, text="Clique aqui", bg='lightblue', fg='black') 
+button2 = tk.Button(janela, text="não clique aqui", bg='pink', fg='black')
+button.grid(row=0, column=0, padx=10, pady=10 )
+button2.grid(row=300, column=1, padx=10, pady=10)
+
+janela.title("oi")
+
+
+janela.mainloop()
